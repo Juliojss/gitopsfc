1b94e44a378ef9458fea4e7f4bf3ea5e4032f36b
Julio Saldanha <juliosaldanha71@gmail.com>
2023-02-12 01:29:59 -0300

    set changes

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello GitOps"))
+		w.Write([]byte("Hello Argo"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
